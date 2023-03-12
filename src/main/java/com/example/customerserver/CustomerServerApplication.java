0be8ae51ad44d163cc100a885cc4a5f2af00f061
impati <impati0716@icloud.com>
2023-03-12 23:05:00 +0900

    첫번째 커밋

---
+++ src/main/java/com/example/customerserver/CustomerServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.customerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/customerserver/CustomerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.customerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
