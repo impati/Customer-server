2670409bc2125fc416fd1c0fb5f7f0c7cb55fc7a
impati <impati0716@icloud.com>
2023-03-13 20:34:52 +0900

    feat : #3 - AppConfig 설정
    스프링에 의존하지 않는 app에서 참조가능한 상수

---
+++ src/main/java/com/example/customerserver/config/AppConfig.java
@@ -0,0 +1,9 @@
+package com.example.customerserver.config;
+
+public abstract class AppConfig {
+
+    public static String getHost(){
+        return "http://impatient.iptime.org:9090";
+    }
+
+}
