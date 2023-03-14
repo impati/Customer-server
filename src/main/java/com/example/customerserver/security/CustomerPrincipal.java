3e761d8575e5295a559e3980240c71fa9d59e4ba
impati <impati0716@icloud.com>
2023-03-14 20:49:37 +0900

    feat : #5 - principal getter 추가

--- src/main/java/com/example/customerserver/security/CustomerPrincipal.java
+++ src/main/java/com/example/customerserver/security/CustomerPrincipal.java
@@ -4,6 +4,7 @@ import com.example.customerserver.domain.Customer;
 import com.example.customerserver.domain.ProviderType;
 import com.example.customerserver.domain.RoleType;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -14,21 +15,58 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Map;
 
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class CustomerPrincipal implements OAuth2User, UserDetails {
 
-    private  Long id;
-    private  String username;
+    private Long id;
+    private String username;
     private String nickname;
-    private  String email;
+    private String email;
     private ProviderType providerType;
-    private  RoleType roleType;
+    private RoleType roleType;
     private Collection<GrantedAuthority> authorities;
     private Map<String, Object> attributes;
-    private  String blogUrl;
-    private  String profileImageUrl;
-    private  String introduceComment;
+    private String blogUrl;
+    private String profileImageUrl;
+    private String introduceComment;
+
+    private CustomerPrincipal(Long id, String username, String nickname,
+                              String email, ProviderType providerType, RoleType roleType,
+                              Collection<GrantedAuthority> authorities, String blogUrl, String profileImageUrl, String introduceComment) {
+        this.id = id;
+        this.username = username;
+        this.nickname = nickname;
+        this.email = email;
+        this.providerType = providerType;
+        this.roleType = roleType;
+        this.authorities = authorities;
+        this.blogUrl = blogUrl;
+        this.profileImageUrl = profileImageUrl;
+        this.introduceComment = introduceComment;
+    }
+
+    public static CustomerPrincipal create(Customer customer) {
+        return new CustomerPrincipal(
+                customer.getId(),
+                customer.getUsername(),
+                customer.getNickname(),
+                customer.getEmail(),
+                customer.getProviderType(),
+                customer.getRoleType(),
+                Collections.singletonList(new SimpleGrantedAuthority(customer.getRoleType().getName())),
+                customer.getBlogUrl(),
+                customer.getProfileImageUrl(),
+                customer.getIntroduceComment()
+        );
+    }
+
+    public static CustomerPrincipal create(Customer customer, Map<String, Object> attributes) {
+        CustomerPrincipal customerPrincipal = create(customer);
+        customerPrincipal.setAttributes(attributes);
+        return customerPrincipal;
+    }
 
     @Override
     public String getPassword() {
@@ -65,6 +103,10 @@ public class CustomerPrincipal implements OAuth2User, UserDetails {
         return attributes;
     }
 
+    private void setAttributes(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return authorities;
@@ -75,53 +117,11 @@ public class CustomerPrincipal implements OAuth2User, UserDetails {
         return nickname;
     }
 
+    public String getEmail() {
+        return email;
+    }
 
-    public String getEmail(){return email;}
-
-
-    public void editNickname(String nickname){
+    public void editNickname(String nickname) {
         this.nickname = nickname;
-    }
-
-    public static CustomerPrincipal create(Customer customer) {
-        return new CustomerPrincipal(
-                customer.getId(),
-                customer.getUsername(),
-                customer.getNickname(),
-                customer.getEmail(),
-                customer.getProviderType(),
-                customer.getRoleType(),
-                Collections.singletonList(new SimpleGrantedAuthority(customer.getRoleType().getName())),
-                customer.getBlogUrl(),
-                customer.getProfileImageUrl(),
-                customer.getIntroduceComment()
-        );
-    }
-
-
-    public static CustomerPrincipal create(Customer customer, Map<String, Object> attributes) {
-        CustomerPrincipal customerPrincipal = create(customer);
-        customerPrincipal.setAttributes(attributes);
-        return customerPrincipal;
-    }
-
-    private void setAttributes(Map<String, Object> attributes) {
-        this.attributes = attributes;
-    }
-
-
-    private CustomerPrincipal(Long id, String username, String nickname,
-                              String email, ProviderType providerType, RoleType roleType,
-                              Collection<GrantedAuthority> authorities, String blogUrl, String profileImageUrl, String introduceComment) {
-        this.id = id;
-        this.username = username;
-        this.nickname = nickname;
-        this.email = email;
-        this.providerType = providerType;
-        this.roleType = roleType;
-        this.authorities = authorities;
-        this.blogUrl = blogUrl;
-        this.profileImageUrl = profileImageUrl;
-        this.introduceComment = introduceComment;
     }
 }
