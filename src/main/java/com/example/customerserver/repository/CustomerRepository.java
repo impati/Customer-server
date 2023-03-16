85e3b621a3f8f4f92deac8b77b1d53568b0ebae9
impati <impati0716@icloud.com>
2023-03-16 19:19:33 +0900

    feat : #11 - 회원가입 valid

--- src/main/java/com/example/customerserver/repository/CustomerRepository.java
+++ src/main/java/com/example/customerserver/repository/CustomerRepository.java
@@ -5,6 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface CustomerRepository extends JpaRepository<Customer,Long> {
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
     Optional<Customer> findByUserId(String userId);
+
+    boolean existsCustomerByUsername(String username);
+
+    boolean existsCustomerByEmail(String email);
 }
---
+++ src/main/java/com/example/customerserver/web/validation/UniqueType.java
@@ -0,0 +1,14 @@
+package com.example.customerserver.web.validation;
+
+import lombok.Getter;
+
+@Getter
+public enum UniqueType {
+    USERNAME("username"), EMAIL("email");
+
+    private final String name;
+
+    UniqueType(String name) {
+        this.name = name;
+    }
+}
